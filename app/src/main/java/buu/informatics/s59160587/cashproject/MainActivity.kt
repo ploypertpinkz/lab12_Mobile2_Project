46fe16955830a7bbffc993c0e705546f1e0afae5
ploypertpinkz <za_magic_keen@hotmail.com>
2019-11-19 21:36:14 +0700

    Add Login Page

--- app/src/main/java/buu/informatics/s59160587/cashproject/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160587/cashproject/MainActivity.kt
@@ -2,11 +2,39 @@ package buu.informatics.s59160587.cashproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val singinBut: Button = findViewById(R.id.singinButton)
+        singinBut.setOnClickListener { gotoOpenBankPage() }
+    }
+
+    private fun gotoOpenBankPage() {
+        if(inputIdText.length() != 0 && inputPassText.length() != 0) {
+            if(inputIdText.text.toString() == "ploy" && inputPassText.text.toString() == "12345") {
+                Toast.makeText(this, "gotoOpenBank", Toast.LENGTH_SHORT).show()
+            } else if(inputIdText.text.toString() != "ploy" && inputPassText.text.toString() == "12345") {
+                Toast.makeText(this, "Username ไม่ถูกต้อง", Toast.LENGTH_SHORT).show()
+            } else if(inputIdText.text.toString() == "ploy" && inputPassText.text.toString() != "12345") {
+                Toast.makeText(this, "Password ไม่ถูกต้อง", Toast.LENGTH_SHORT).show()
+            } else if(inputIdText.text.toString() != "ploy" && inputPassText.text.toString() != "12345") {
+                Toast.makeText(this, "Username เเละ Password ไม่ถูกต้อง", Toast.LENGTH_SHORT).show()
+            }
+
+        } else if(inputIdText.length() == 0 && inputPassText.length() != 0) {
+            Toast.makeText(this, "กรุณากรอก Username", Toast.LENGTH_SHORT).show()
+        }else if(inputIdText.length() != 0 && inputPassText.length() == 0) {
+            Toast.makeText(this, "กรุณากรอก Password", Toast.LENGTH_SHORT).show()
+        } else if(inputIdText.length() == 0 && inputPassText.length() == 0) {
+            Toast.makeText(this, "กรุณากรอก Username เเละ Password", Toast.LENGTH_SHORT).show()
+        }
     }
 }
